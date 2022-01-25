122764da5849c9a46cb4231903aa70b7c873f95b
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-01-25 20:27:39 +0900

    Challenge: Track the question number using variable

--- lib/main.dart
+++ lib/main.dart
@@ -26,11 +26,14 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
+
   List<String> questions = [
     'You can lead a cow down stairs but not up stairs.',
     'Approximately one quarter of human bones are in the feet.',
     'A slug\'s blood is green.',
   ];
+
+  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[0],
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
