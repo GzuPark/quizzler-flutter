50b9bb40de55b44022f1f8ad2c7336bea1ca38f5
Jongmin Park <9290341+GzuPark@users.noreply.github.com>
2022-01-26 20:28:13 +0900

    Challenge: Apply class capsulation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:quizzler/question.dart';
+import 'quiz_brain.dart';
 
 void main() => runApp(Quizzler());
 
@@ -28,11 +28,7 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<Question> questions = [
-    Question(questionText: 'You can lead a cow down stairs but not up stairs.', questionAnswer: false),
-    Question(questionText: 'Approximately one quarter of human bones are in the feet.', questionAnswer: true),
-    Question(questionText: 'A slug\'s blood is green.', questionAnswer: true),
-  ];
+  QuestionBrain questionBrain = QuestionBrain();
 
   int questionNumber = 0;
 
@@ -48,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber].questionText,
+                questionBrain.questions[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -73,6 +69,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = questionBrain.questions[questionNumber].questionAnswer;
+
+                if (correctAnswer == true) {
+                  print('Right');
+                } else {
+                  print('Wrong');
+                }
+
                 setState(() {
                   scoreKeeper.add(
                     Icon(
@@ -101,6 +105,14 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
+                bool correctAnswer = questionBrain.questions[questionNumber].questionAnswer;
+
+                if (correctAnswer != true) {
+                  print('Right');
+                } else {
+                  print('Wrong');
+                }
+
                 setState(() {
                   scoreKeeper.add(
                     Icon(
